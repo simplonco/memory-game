f1d82d688396d982e61b689e1ea731dcc6c14c5d
solomongamid <sulimanabdulgadeir@gmail.com>
2016-08-04 19:19:37 +0200

    new version

--- css-animation.js
+++ css-animation.js
@@ -1,87 +1,118 @@
+// Original JavaScript code by Chirp Internet: www.chirp.com.au
+// Please acknowledge use of this code by including this header.
 
-
-  // Original JavaScript code by Chirp Internet: www.chirp.com.au
-  // Please acknowledge use of this code by including this header.
-
-  var card_value = ['1C','2C','3C','4C','5C','6C','7C','8C','1H','2H','3H','4H','5H','6H','7H','8H'];
-
-  // set default positions
-  var card_left = [];
-  var card_top = [];
-
-  for(var i=0; i < 16; i++) {
-    card_left[i] = 70 + 100 * (i%4);
-    card_top[i] = 15 + 120 * Math.floor(i/4);
-  }
+var CardGame = function(targetId)
+{
+  // private variables
+  var cards = []
+  var card_value = ["1C","2C","3C","4C","5C","6C","7C","8C","1H","2H","3H","4H","5H","6H","7H","8H"];
 
   var started = false;
-  var cards_turned = 0;
   var matches_found = 0;
-  var card1 = false;
-  var card2 = false;
+  var card1 = false, card2 = false;
 
-  function moveToPlace(id)
+  var hideCard = function(id) // turn card face down
   {
-    var el = document.getElementById("card_" + id);
-    el.style["z-index"] = "1000";
-    el.style["left"] = card_left[id] + "px";
-    el.style["top"] = card_top[id] + "px";
-    el.style["z-index"] = "0";
-  }
+    cards[id].firstChild.src = "images/cards/back.png";
+    with(cards[id].style) {
+      WebkitTransform = MozTransform = OTransform = msTransform = "scale(1.0) rotate(180deg)";
+    }
+  };
 
-  function hideCard(id)
-  {
-    var el = document.getElementById("card_" + id);
-    el.firstChild.src = "images/cards/back.png";
-    el.style["WebkitTransform"] = "scale(1.0)";
-  }
-
-  function moveToPack(id)
+  var moveToPack = function(id) // move card to pack
   {
     hideCard(id);
-    var el = document.getElementById("card_" + id);
-    el.style["z-index"] = "1000";
-    el.style["left"] = "-140px";
-    el.style["top"] = "100px";
-    el.style["z-index"] = "0";
-  }
+    cards[id].matched = true;
+    with(cards[id].style) {
+      zIndex = "1000";
+      top = "100px";
+      left = "-140px";
+      WebkitTransform = MozTransform = OTransform = msTransform = "rotate(0deg)";
+      zIndex = "0";
+    }
+  };
 
-  // flip over card and check for match
-  function showCard(id)
+  var moveToPlace = function(id) // deal card
+  {
+    cards[id].matched = false;
+    with(cards[id].style) {
+      zIndex = "1000";
+      top = cards[id].fromtop + "px";
+      left = cards[id].fromleft + "px";
+      WebkitTransform = MozTransform = OTransform = msTransform = "rotate(180deg)";
+      zIndex = "0";
+    }
+  };
+
+  var showCard = function(id) // turn card face up, check for match
   {
     if(id === card1) return;
-    var el = document.getElementById("card_" + id);
-    el.firstChild.src = card_value[id] + ".png";
-    el.style["WebkitTransform"] = "scale(1.2)";
-    if(++cards_turned == 2) {
+    if(cards[id].matched) return;
+
+    cards[id].firstChild.src = "images/cards/" + card_value[id] + ".png";
+    with(cards[id].style) {
+      WebkitTransform = MozTransform = OTransform = msTransform = "scale(1.2) rotate(185deg)";
+    }
+
+    if(card1 !== false) {
       card2 = id;
-      // check whether both cards have the same value
-      if(parseInt(card_value[card1]) == parseInt(card_value[card2])) {
-        setTimeout("moveToPack(" + card1 + "); moveToPack(" + card2 + ");", 1000);
-        if(++matches_found == 8) {
-          // game over
+      if(parseInt(card_value[card1]) == parseInt(card_value[card2])) { // match found
+        (function(card1, card2) {
+          setTimeout(function() { moveToPack(card1); moveToPack(card2); }, 1000);
+        })(card1, card2);
+        if(++matches_found == 8) { // game over, reset
           matches_found = 0;
           started = false;
         }
-      } else {
-        setTimeout("hideCard(" + card1 + "); hideCard(" + card2 + ");", 800);
+      } else { // no match
+        (function(card1, card2) {
+          setTimeout(function() { hideCard(card1); hideCard(card2); }, 800);
+        })(card1, card2);
       }
       card1 = card2 = false;
-      cards_turned = 0;
-    } else {
+    } else { // first card turned over
       card1 = id;
     }
-  }
+  };
 
-  function cardClick(id)
+  var cardClick = function(id)
   {
     if(started) {
       showCard(id);
     } else {
       // shuffle and deal cards
       card_value.sort(function() { return Math.round(Math.random()) - 0.5; });
-      for(i=0; i < 16; i++) setTimeout("moveToPlace(" + i + ")", i * 100);
+      for(i=0; i < 16; i++) {
+        (function(idx) {
+          setTimeout(function() { moveToPlace(idx); }, idx * 100);
+        })(i);
+      }
       started = true;
     }
+  };
+
+  // initialise
+
+  var stage = document.getElementById(targetId);
+  var felt = document.createElement("div");
+  felt.id = "felt";
+  stage.appendChild(felt);
+
+  // template for card
+  var card = document.createElement("div");
+  card.innerHTML = "<img src=\"images/cards/back.png\">";
+
+  for(var i=0; i < 16; i++) {
+    var newCard = card.cloneNode(true);
+
+    newCard.fromtop = 15 + 120 * Math.floor(i/4);
+    newCard.fromleft = 70 + 100 * (i%4);
+    (function(idx) {
+      newCard.addEventListener("click", function() { cardClick(idx); }, false);
+    })(i);
+
+    felt.appendChild(newCard);
+    cards.push(newCard);
   }
 
+}
